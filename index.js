d33a8cb2b012050ff6523271e660fe1ef95fb7f7
NelsonSenhouse <144487032+NelsonSenhouse@users.noreply.github.com>
2025-01-16 00:13:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* === Imports === */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
 /* === Firebase Setup === */
+const firebaseConfig = {
+    apiKey: "AIzaSyBa-wRW2iRnlsneuf8FiI6woBiVYlXQMCg",
+    authDomain: "hot-and-cold-26728.firebaseapp.com",
+    projectId: "hot-and-cold-26728",
+    storageBucket: "hot-and-cold-26728.firebasestorage.app",
+    messagingSenderId: "394606284456",
+    appId: "1:394606284456:web:7b66af2c81a0ffa87e7c9d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const user = auth.currentUser;
+
 
 /* === UI === */
 
@@ -17,6 +36,12 @@ const passwordInputEl = document.getElementById("password-input")
 const signInButtonEl = document.getElementById("sign-in-btn")
 const createAccountButtonEl = document.getElementById("create-account-btn")
 
+const signOutButtonEl = document.getElementById("sign-out-btn")
+
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+
+const userGreetingEl = document.getElementById("user-greeting")
+
 /* == UI - Event Listeners == */
 
 signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
@@ -24,11 +49,37 @@ signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 signInButtonEl.addEventListener("click", authSignInWithEmail)
 createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
 
+signOutButtonEl.addEventListener("click", authSignOut)
+
 /* === Main Code === */
 
 showLoggedOutView()
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      showLoggedInView()
+      const uid = user.uid;
+      showProfilePicture(userProfilePictureEl, user);
+      showUserGreeting(userGreetingEl, user)
+    } 
+    else {
+      showLoggedOutView()
+    }
+});
+   
+ 
 
 /* === Functions === */
+
+function showProfilePicture(imgElement, user) {
+    if (user && user.profilePictureURL) {
+        imgElement.src = user.profilePictureURL;
+    } 
+    else {
+        imgElement.src = "assets/images/defaultPic.jpg";
+    }
+
+}
+ 
 
 /* = Functions - Firebase - Authentication = */
 
@@ -38,30 +89,73 @@ function authSignInWithGoogle() {
 
 function authSignInWithEmail() {
     console.log("Sign in with email and password")
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        showLoggedInView()
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
 }
 
 function authCreateAccountWithEmail() {
     console.log("Sign up with email and password")
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        showLoggedInView()
+    })
+    .catch((error) => {
+        console.error(error.message)
+    });
 }
+
+function authSignOut() {
+    signOut(auth)
+    .then(() => {
+        showLoggedOutView()
+    })
+    .catch((error) => {
+        console.error(error.message)
+        });
+}
+ 
 
 /* == Functions - UI Functions == */
 
 function showLoggedOutView() {
-    hideElement(viewLoggedIn)
-    showElement(viewLoggedOut)
-}
+    hideView(viewLoggedIn)
+    showView(viewLoggedOut)
+} 
 
 function showLoggedInView() {
-    hideElement(viewLoggedOut)
-    showElement(viewLoggedIn)
-}
+    hideView(viewLoggedOut)
+    showView(viewLoggedIn)
+} 
 
-function showElement(element) {
-    element.style.display = "flex"
-}
+function showView(view) {
+    view.style.display = "flex"
+} 
 
-function hideElement(element) {
-    element.style.display = "none"
-}
+function hideView(view) {
+    view.style.display = "none"
+} 
 
 //credit: coursera
+
+function showUserGreeting(element, user) {
+    const displayName = user.displayName;
+    console.log(displayName)
+    if (user.displayName) {
+        const firstName = user.displayName.split(' ')[0];
+        element.textContent = `Hi ${firstName}, how are you?`;
+    } 
+    else {
+        element.textContent = "Hey friend, how are you?";
+    }
+}
